3f25cf60f0539a8b1638f4b5f9c2eff87d2d942a
RodrigoChousal <rodrigochousal@gmail.com>
2018-11-19 11:05:02 -0600

    Initial Commit

---
+++ NuevoAmanecer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NuevoAmanecer
+//
+//  Created by Rodrigo Chousal on 11/19/18.
+//  Copyright © 2018 Rodrigo Chousal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
