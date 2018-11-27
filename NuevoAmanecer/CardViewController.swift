cd5df30656010a099557f3f7b6510777649cb0d6
RodrigoChousal <rodrigochousal@gmail.com>
2018-11-27 15:13:33 -0600

    Constraints and other improvements

--- NuevoAmanecer/CardViewController.swift
+++ NuevoAmanecer/CardViewController.swift
@@ -47,10 +47,12 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 		if collectionView == self.cardCollectionView {
 			if self.isHome {
 				let card = cards[indexPath.row] as! CategoryCard
+				print("Selected category cell: " + card.title)
 				showWords(fromCategoryCard: card)
 			} else {
-				let card = cards[indexPath.row] as! WordCard
-				addWord(fromWordCard: card)
+				print("Selected word cell")
+//				let card = cards[indexPath.row] as! WordCard
+//				addWord(fromWordCard: card)
 			}
 		} else { // Selected word list
 			// Open word list
@@ -74,7 +76,7 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 			cell.imageView.image = self.cards[indexPath.row].image
 			return cell
 		} else {
-			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectedWordCell", for: indexPath) as! CardCollectionViewCell
+			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectedWordCell", for: indexPath) as! SelectedWordCollectionViewCell
 			cell.titleLabel.text = self.selectedWords[indexPath.row].title
 			return cell
 		}
@@ -89,6 +91,7 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 	}
 	
 	@IBAction func homePressed(_ sender: Any) {
+		print("Pressed home")
 		self.isHome = true
 		self.homeButton.isHidden = true
 		self.cards = vocabulary.categoryCards
@@ -107,8 +110,10 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 	// MARK: - Helper Methods
 	
 	func showWords(fromCategoryCard categoryCard: CategoryCard) {
+		print("Showing words...")
 		self.isHome = false
 		self.homeButton.isHidden = false
+		print("Showed home button")
 		cards = categoryCard.wordCards
 		self.cardCollectionView.reloadData()
 	}
@@ -119,33 +124,33 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 	}
 	
 	func makeButtonsPretty() {
-		backButton.layer.cornerRadius = 50
+		backButton.layer.cornerRadius = backButton.frame.width*0.2
 		backButton.layer.borderWidth = 5
-		backButton.layer.borderColor = (UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0) as! CGColor)
+		backButton.layer.borderColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0).cgColor
 		backButton.backgroundColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
 		backButton.clipsToBounds = true
 		
-		nextButton.layer.cornerRadius = 50
+		nextButton.layer.cornerRadius = nextButton.frame.width*0.2
 		nextButton.layer.borderWidth = 5
-		nextButton.layer.borderColor = (UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0) as! CGColor)
+		nextButton.layer.borderColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0).cgColor
 		nextButton.backgroundColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
 		nextButton.clipsToBounds = true
 		
-		homeButton.layer.cornerRadius = homeButton.frame.width/2
+		homeButton.layer.cornerRadius = homeButton.frame.width*0.5
 		homeButton.layer.borderWidth = 5
-		homeButton.layer.borderColor = (UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0) as! CGColor)
+		homeButton.layer.borderColor = UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0).cgColor
 		homeButton.backgroundColor = UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0)
 		homeButton.clipsToBounds = true
 		
 		sayButton.layer.cornerRadius = 50
 		sayButton.layer.borderWidth = 5
-		sayButton.layer.borderColor = (UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0) as! CGColor)
+		sayButton.layer.borderColor = UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0).cgColor
 		sayButton.backgroundColor = UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0)
 		sayButton.clipsToBounds = true
 		
 		deleteButton.layer.cornerRadius = 50
 		deleteButton.layer.borderWidth = 5
-		deleteButton.layer.borderColor = (UIColor(red: 192/255, green: 57/255, blue: 43/255, alpha: 1.0) as! CGColor)
+		deleteButton.layer.borderColor = UIColor(red: 192/255, green: 57/255, blue: 43/255, alpha: 1.0).cgColor
 		deleteButton.backgroundColor = UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0)
 		deleteButton.clipsToBounds = true
 	}
--- NuevoAmanecer/INAModel.swift
+++ NuevoAmanecer/INAModel.swift
@@ -9,8 +9,52 @@
 import Foundation
 import UIKit
 
+var image = UIImage(named: "placeholder") ?? UIImage()
+
 class Vocabulary {
-	var categoryCards = [CategoryCard]()
+	
+	var categoryCards = [CategoryCard(title: "Sentimientos",
+									  image: image,
+									  wordCards: [WordCard(title: "Enojado", image: image),
+												  WordCard(title: "Feliz", image: image),
+												  WordCard(title: "Triste", image: image),
+												  WordCard(title: "Emocionado", image: image)]),
+						 CategoryCard(title: "Personas",
+									  image: image,
+									  wordCards: [WordCard(title: "Papá", image: image),
+												  WordCard(title: "Mamá", image: image),
+												  WordCard(title: "Hermano", image: image),
+												  WordCard(title: "Primo", image: image)]),
+						 CategoryCard(title: "Objetos",
+									  image: image,
+									  wordCards: [WordCard(title: "Pelota", image: image),
+												  WordCard(title: "Tablet", image: image),
+												  WordCard(title: "Lápiz", image: image),
+												  WordCard(title: "Mochila", image: image)]),
+						 CategoryCard(title: "Comida",
+									  image: image,
+									  wordCards: [WordCard(title: "Pizza", image: image),
+												  WordCard(title: "Elote", image: image),
+												  WordCard(title: "Nieve", image: image),
+												  WordCard(title: "Brocoli", image: image)]),
+						 CategoryCard(title: "Colores",
+									  image: image,
+									  wordCards: [WordCard(title: "Azul", image: image),
+												  WordCard(title: "Verde", image: image),
+												  WordCard(title: "Negro", image: image),
+												  WordCard(title: "Naranja", image: image)]),
+						 CategoryCard(title: "Números",
+									  image: image,
+									  wordCards: [WordCard(title: "Nueve", image: image),
+												  WordCard(title: "Ocho", image: image),
+												  WordCard(title: "Siete", image: image),
+												  WordCard(title: "Seis", image: image)]),
+						 CategoryCard(title: "Letras",
+									  image: image,
+									  wordCards: [WordCard(title: "A", image: image),
+												  WordCard(title: "B", image: image),
+												  WordCard(title: "C", image: image),
+												  WordCard(title: "D", image: image)])]
 }
 
 class Card {
