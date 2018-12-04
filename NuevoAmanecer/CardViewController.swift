f0b41060d731a43fd10f537e8c29d768f6afbcd4
RodrigoChousal <rodrigochousal@gmail.com>
2018-12-04 09:13:52 -0600

    Added AudioKit, and cocoa pods.

--- NuevoAmanecer/CardViewController.swift
+++ NuevoAmanecer/CardViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 var vocabulary = Vocabulary()
 
@@ -21,6 +22,8 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 	@IBOutlet weak var sayButton: UIButton!
 	@IBOutlet weak var deleteButton: UIButton!
 	@IBOutlet weak var wordCollectionView: UICollectionView!
+	
+	var speechSynth = AVSpeechSynthesizer()
 	
 	var isHome = true
 	var cards = [Card]()
@@ -37,6 +40,13 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
         wordCollectionView.delegate = self
 		wordCollectionView.dataSource = self
 		
+		prepareRecorder()
+		
+		wordCollectionView.layer.cornerRadius = 12
+		wordCollectionView.layer.borderWidth = 5
+		wordCollectionView.layer.borderColor = UIColor(red: 243/255, green: 156/255, blue: 17/255, alpha: 1.0).cgColor
+		wordCollectionView.backgroundColor = UIColor(red: 241/255, green: 195/255, blue: 15/255, alpha: 1.0)
+		
 		if isHome { homeButton.isHidden = true }
 		makeButtonsPretty()
     }
@@ -47,12 +57,10 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 		if collectionView == self.cardCollectionView {
 			if self.isHome {
 				let card = cards[indexPath.row] as! CategoryCard
-				print("Selected category cell: " + card.title)
 				showWords(fromCategoryCard: card)
 			} else {
-				print("Selected word cell")
-//				let card = cards[indexPath.row] as! WordCard
-//				addWord(fromWordCard: card)
+				let card = cards[indexPath.row] as! WordCard
+				addWord(fromWordCard: card)
 			}
 		} else { // Selected word list
 			// Open word list
@@ -78,6 +86,8 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 		} else {
 			let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SelectedWordCell", for: indexPath) as! SelectedWordCollectionViewCell
 			cell.titleLabel.text = self.selectedWords[indexPath.row].title
+			cell.layer.borderWidth = 5
+			cell.layer.borderColor = UIColor(red: 243/255, green: 156/255, blue: 17/255, alpha: 1.0).cgColor
 			return cell
 		}
 	}
@@ -85,36 +95,74 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 	// MARK: - Action Methods
 	
 	@IBAction func backPressed(_ sender: Any) {
+		let offsetPerCard = cardCollectionView.contentSize.width/CGFloat(cards.count)
+		let newOffset = cardCollectionView.contentOffset.x - offsetPerCard
+		if newOffset > 0 {
+			cardCollectionView.contentOffset.x -= offsetPerCard
+		}
 	}
 	
 	@IBAction func nextPressed(_ sender: Any) {
+		let scrollWidth = cardCollectionView.contentSize.width
+		let offsetPerCard = cardCollectionView.contentSize.width/CGFloat(cards.count)
+		let newOffset = cardCollectionView.contentOffset.x + offsetPerCard
+		if newOffset < scrollWidth {
+			cardCollectionView.contentOffset.x += offsetPerCard
+		}
 	}
 	
 	@IBAction func homePressed(_ sender: Any) {
-		print("Pressed home")
 		self.isHome = true
 		self.homeButton.isHidden = true
 		self.cards = vocabulary.categoryCards
+		self.cardCollectionView.contentOffset.x = 0
 		self.cardCollectionView.reloadData()
 	}
 	
 	@IBAction func sayPressed(_ sender: Any) {
+		for word in selectedWords {
+			let utterance = AVSpeechUtterance(string: word.title)
+			utterance.voice = AVSpeechSynthesisVoice(language: "es-MX")
+			speechSynth.speak(utterance)
+		}
 	}
 	
 	@IBAction func deletePressed(_ sender: Any) {
-		let lastIndex = IndexPath(item: selectedWords.count-1, section: 0)
-		self.selectedWords.remove(at: lastIndex.row)
-		self.wordCollectionView.deleteItems(at: [lastIndex])
+		if selectedWords.count != 0 {
+			let lastIndex = IndexPath(item: selectedWords.count-1, section: 0)
+			self.selectedWords.remove(at: lastIndex.row)
+			self.wordCollectionView.deleteItems(at: [lastIndex])
+		}
 	}
 	
 	// MARK: - Helper Methods
 	
+	func prepareRecorder() {
+		
+		var recordingSession = AVAudioSession.sharedInstance()
+		
+		do {
+			try recordingSession.setCategory(.playAndRecord, mode: .default)
+			try recordingSession.setActive(true)
+			recordingSession.requestRecordPermission() { [unowned self] allowed in
+				DispatchQueue.main.async {
+					if allowed {
+						self.loadRecordingUI()
+					} else {
+						self.loadFailUI()
+					}
+				}
+			}
+		} catch {
+			self.loadFailUI()
+		}
+	}
+	
 	func showWords(fromCategoryCard categoryCard: CategoryCard) {
-		print("Showing words...")
 		self.isHome = false
 		self.homeButton.isHidden = false
-		print("Showed home button")
 		cards = categoryCard.wordCards
+		self.cardCollectionView.contentOffset.x = 0
 		self.cardCollectionView.reloadData()
 	}
 	
@@ -136,7 +184,7 @@ class CardViewController: UIViewController, UICollectionViewDelegate, UICollecti
 		nextButton.backgroundColor = UIColor(red: 52/255, green: 152/255, blue: 219/255, alpha: 1.0)
 		nextButton.clipsToBounds = true
 		
-		homeButton.layer.cornerRadius = homeButton.frame.width*0.5
+		homeButton.layer.cornerRadius = homeButton.frame.width*0.2
 		homeButton.layer.borderWidth = 5
 		homeButton.layer.borderColor = UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1.0).cgColor
 		homeButton.backgroundColor = UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1.0)
